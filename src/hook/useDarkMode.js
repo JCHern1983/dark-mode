41b51b54460b1e30ee469f663d18b93e1cfb91b8
JCHern1983 <xnemesis21@gmail.com>
2020-07-08 16:02:12 -0700

    MVP

---
+++ src/hook/useDarkMode.js
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+import { useLocalStorage } from "./useLocalStorage";
+
+export const useDarkMode = (key)=>{
+    const [darkmode, setDarkMode] = useLocalStorage(key);
+    useEffect(()=>{
+        if(darkmode === true){
+        document.body.classList.add('dark-mode')
+        }
+        else{
+            document.body.classList.remove('dark-mode')  
+        }
+    }, [darkmode])
+    return [darkmode, setDarkMode];
+
+} 
+export default useDarkMode;
